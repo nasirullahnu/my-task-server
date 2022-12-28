f95bc4935527700e7ef635800059ad9d86c7fead
nasirullahnu <nasirullah878@gmail.com>
2022-12-28 21:10:49 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -13,8 +13,17 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5uqgn7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+        try{
+            const taskCollection = client.db('myTask').collection('allTask')
 
 
+        }
+        finally{
+
+        }
+}
+run().catch(error => console.error(error))
 
 
 
