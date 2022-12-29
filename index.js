0966abe7a6e036d5dd7643bee9fa7fb4bc61c2a0
nasirullahnu <nasirullah878@gmail.com>
2022-12-29 16:04:49 +0600

    task delete and complete api is created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,14 +25,42 @@ async function run(){
             res.send(result) 
             })
 
+            // load user specific task 
             app.get('/myTask', async (req, res)=> {
                 const email = req.query.email
+                // const status = req.query.status
                 const query = {
-                    userEmail : email
+                    userEmail : email,
+                    // status : status
                 };
                 const result = await taskCollection.find(query).toArray();
                 res.send(result)
             })
+
+
+            app.delete('/myTask/:id', async (req, res)=> {
+                const id = req.params.id
+                console.log(id)
+                const filter = {_id : ObjectId(id)}
+                const result = await taskCollection.deleteOne(filter);
+                res.send(result);
+            })
+
+
+            // make admin api is created 
+        app.put('/myTask/:id',  async (req, res)=> {
+            const id = req.params.id
+            console.log(id)
+            const filter = {_id : ObjectId(id)}
+            const option = {upsert : true};
+            const updatedDoc = {
+                        $set : {
+                            status : 'completed'
+                        }
+                    }
+                    const result = await taskCollection.updateOne(filter, updatedDoc, option)
+            res.send(result);
+        })
 
 
 
